0cc218e33fec86c1e12574879de1d6adf210fa6f
yatrikm98 <yatrikmehta82@gmail.com>
2023-02-22 14:23:20 +0530

    I am uploading files

---
+++ dom.js
@@ -0,0 +1,26 @@
+var body=document.getElementById("gradient")
+var color1=document.querySelector(".color1")
+var color2=document.querySelector(".color2")
+var css=document.querySelector("h3")
+
+
+color1.addEventListener("input",function(){
+	body.style.background="linear-gradient(to right,"
+	+ color1.value
+	+","
+	+ color2.value
+	+")"
+
+	css.textContent=body.style.background +";"
+})
+
+
+color2.addEventListener("input",function(){
+	body.style.background="linear-gradient(to right,"
+	+ color1.value
+	+","
+	+ color2.value
+	+")"
+
+	css.textContent=body.style.background +";";
+})
